9e97c774d9038d812013f6831d1949cf6142d961
Danial <admin@redsparr0w.com>
2025-06-10 19:26:33 +1200

    .

--- src/main/java/com/tormenteddemons/TormentedDemonsPlugin.java
+++ src/main/java/com/tormenteddemons/TormentedDemonsPlugin.java
@@ -4,6 +4,7 @@ import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
+import net.runelite.api.GameState;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
