1b4bfd4cf5affc69d65ab015157d8f0be9cf03f3
Danial <admin@redsparr0w.com>
2025-06-24 12:16:53 +1200

    Add comment

--- src/main/java/com/killcountviewer/KillCountViewerOverlay.java
+++ src/main/java/com/killcountviewer/KillCountViewerOverlay.java
@@ -323,6 +323,8 @@ public class KillCountViewerOverlay extends Overlay
 			final int textHeight = graphics.getFontMetrics().getHeight() - graphics.getFontMetrics().getMaxDescent();
 			final int textWidth = graphics.getFontMetrics().stringWidth(killCountText);
 			final int imageMargin = 5;
+
+			// If we only have a left icon, center the whole text block
 			if (rightIcon == null)
 			{
 				textLocation = new Point(textLocation.getX() + (imageMargin + leftIcon.getWidth(null)) / 2, textLocation.getY());
